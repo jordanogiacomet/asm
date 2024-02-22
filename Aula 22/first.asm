e4b668a139a271f151f40060ef91955abbf2fa83
jordanogiacomet <110415222+jordanogiacomet@users.noreply.github.com>
2024-02-22 13:16:25 -0300

    WIP - Aula 22 completa;

--- Aula 22/first.asm
+++ Aula 22/first.asm
@@ -1,6 +1,7 @@
 section .data
         pathname DD "/home/jordano/Documents/test.txt"
-global .text
+        toWrite DD "Hello World!",0AH,0DH,"$"
+section .text
 global main
 
 main:
@@ -8,4 +9,13 @@ main:
     mov ebx,pathname
     mov ecx,101o
     mov edx,700o
+    int 80h
+    
+    mov ebx,eax
+    mov eax,4
+    mov ecx,toWrite
+    mov edx,15
+    
+    mov eax,1
+    xor ebx,ebx
     int 80h
